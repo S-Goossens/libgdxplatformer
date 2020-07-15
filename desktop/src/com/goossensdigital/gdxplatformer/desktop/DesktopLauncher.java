2e89b07caa03b46dfd812e69192619cdc4705d8f
Sidney Goossens <sidney.goossens96@gmail.com>
2020-07-15 14:20:18 +0200

    initial commit

---
+++ desktop/src/com/goossensdigital/gdxplatformer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.goossensdigital.gdxplatformer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.goossensdigital.gdxplatformer.Gdxplatformer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Gdxplatformer(), config);
+	}
+}
